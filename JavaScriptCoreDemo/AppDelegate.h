c1015df57faaf74306227b1d73eddb42fbb1d4ac
reborn <343633674@qq.com>
2016-09-26 10:11:22 +0800

    Initial Commit

---
+++ JavaScriptCoreDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JavaScriptCoreDemo
+//
+//  Created by 马英 on 16/9/26.
+//  Copyright © 2016年 reborn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JavaScriptCoreDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JavaScriptCoreDemo
+//
+//  Created by 马英 on 16/9/26.
+//  Copyright © 2016年 reborn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
